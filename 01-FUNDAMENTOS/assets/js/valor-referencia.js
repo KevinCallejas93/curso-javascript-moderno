c9fd84440ec2e155fb169a9607e83032a1047aa8
KevinCallejas93 <kevinalex93@gmail.com>
2026-06-04 17:50:52 -0600

    Finalizamos la parte de valor referencia del curso

--- 01-FUNDAMENTOS/assets/js/valor-referencia.js
+++ 01-FUNDAMENTOS/assets/js/valor-referencia.js
@@ -10,20 +10,36 @@ console.log({ a, b });
 
 //Pasar por referencia
 let juan = { nombre: 'Juan' };
-let ana  = juan;
-
+let ana  = {...juan};
 ana.nombre = 'Ana';
 
 console.log({ juan, ana });
 
 
-const cambiaNombre = ( persona ) => {
+const cambiaNombre = ( {...persona} ) => {
   persona.nombre = 'Kevin';
   return persona;
 
 };
 
 let peter = { nombre: 'Peter' };
-let kevin = cambiaNombre( peter );
+let kevin = cambiaNombre(peter );
 
 console.log({ peter, kevin });
+
+
+//Arreglos
+const frutas = ['Manzana', 'Pera', 'Piña'];
+
+//forma de probar el tiempo que tarda cada método para hacer una copia del arreglo, slice o spread
+console.time('slice');
+const otrasFrutas = frutas.slice(); //otra forma de hacer una copia es con el operador spread: const otrasFrutas = [...frutas];
+console.timeEnd('slice');
+
+console.time('Spread');
+const otrasFrutas2 = [...frutas];
+console.timeEnd('Spread');
+
+otrasFrutas.push('Mango');
+
+console.table({ frutas, otrasFrutas });
