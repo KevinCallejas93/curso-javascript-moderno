03db4ed08a452e6f874d6dbf6944f8a23ab4dfe2
KevinCallejas93 <kevinalex93@gmail.com>
2026-06-04 19:11:04 -0600

    Agregamos el archivo if-else.js

---
+++ 01-FUNDAMENTOS/assets/js/if-else.js
@@ -0,0 +1,53 @@
+
+
+let a = 5;
+
+if ( a >= 10 ) {
+    console.log('a es mayor o igual que 10');
+}
+else {
+    console.log('a es menor que 10');
+}
+
+
+// console.log( 'Fin del programa' );
+
+const hoy = new Date();
+
+let dia = hoy.getDay(); // 0: Domingo, 1: Lunes, ..., 6: Sábado
+
+console.log( {dia} );
+
+if ( dia === 0 ) {
+    console.log('Domingo');
+} else if ( dia === 1 ) {
+    console.log('Lunes');
+} else if ( dia === 2 ) {
+    console.log('Martes');
+} else {
+    console.log('No es ni Domingo, ni Lunes, ni Martes');
+}
+
+// Tarea: Sin usar if-else, o switch, imprimir el día de la semana utilizando unicamente objetos
+
+//Primer forma: Objeto literal
+const dias ={
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miércoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado',
+}
+
+console.log( dias [1] );
+
+
+//Segunda forma: Array
+const diaSemana = [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ];
+
+console.log( diaSemana [3] );
+
+
+
