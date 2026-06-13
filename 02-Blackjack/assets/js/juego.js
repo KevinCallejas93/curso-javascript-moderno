75a3e451ee18d8b90525523885af54690e9db620
KevinCallejas93 <kevinalex93@gmail.com>
2026-06-13 00:00:33 -0600

    Trabajamos la funcionalidad completa de juego.js

--- 02-Blackjack/assets/js/juego.js
+++ 02-Blackjack/assets/js/juego.js
@@ -1,2 +1,74 @@
+/**
+ * 2C = Two of Clubs 
+ * 2D = Two of Diamonds 
+ * 2H = Two of Hearts 
+ * 2S = Two of Spades 
+ */
 
 
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+
+// Esta funcion crea una nueva baraja desordenada
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+
+    }
+
+    for (const tipo of tipos) {
+        for (const esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+    
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+};
+
+crearDeck();
+
+//Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    
+    const carta = deck.pop(); // .pop remueve la ultima carta y la devuelve
+    console.log( deck );
+    console.log( carta );
+    return carta;
+}
+
+const valorCarta = ( carta ) => {
+    
+    const valor = carta.substring(0, carta.length - 1);
+    // let puntos = 0;
+
+    console.log( valor );
+    
+    //Tarea Reducir esto con operador ternario
+    // if ( isNaN( valor ) ) {
+        //     puntos = ( valor === 'A' ) ? 11 : 10;
+        // } else {
+            //     puntos = valor * 1;
+            // }
+            
+    //Operador Ternario
+    return ( isNaN( valor ) ) ? 
+        ( valor === 'A' ) ? 11 : 10
+        : valor * 1;
+          
+}
+
+const valor = valorCarta( pedirCarta() ); //Aqui se manda a llamar la funcion pedirCarta, y directamente envia la carta a valorCarta.
+
+
+console.log( valor );
