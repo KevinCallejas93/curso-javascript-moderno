9bdb6d02b96d489a4e6d6a1aec887818f5db6136
KevinCallejas93 <kevinalex93@gmail.com>
2026-06-14 12:20:59 -0600

    Hicimos la logica del evento del boton Detener

--- 02-Blackjack/assets/js/juego.js
+++ 02-Blackjack/assets/js/juego.js
@@ -78,6 +78,32 @@ const valorCarta = ( carta ) => {
           
 }
 
+//Turno de la computadora
+
+const turnoComputadora = ( puntosMinimos ) => {
+
+    do {
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+
+        puntosHTML[1].innerText = puntosComputadora;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `/02-Blackjack/assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+
+        divCartasComputadora.append( imgCarta );
+
+        if ( puntosMinimos > 21 ) {
+            break;
+        }
+
+
+    } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+};
+
 const valor = valorCarta( pedirCarta() ); //Aqui se manda a llamar la funcion pedirCarta, y directamente envia la carta a valorCarta.
 
 
@@ -101,15 +127,22 @@ btnPedir.addEventListener('click', () => {
         console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
+        turnoComputadora( puntosJugador ); 
         
     } else if ( puntosJugador === 21 ) {
         console.warn('21, genial!');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
+        turnoComputadora( puntosJugador ); 
     }
-
     
 });
 
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora( puntosJugador ); 
+});
 
-    
+
+
